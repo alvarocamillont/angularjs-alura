98d5418f639cd44c44c3bd4546e29c2fc734ab3a
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-04-25 13:46:41 -0300

    Estudo inicial do ng-repeat

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,17 @@
 /* global angular */
 
 angular.module('alurapic').controller('FotosController', function ($scope) {
-  $scope.foto = {
-    titulo: 'Leão',
+  $scope.fotos = [{
+    titulo: 'Leao',
+    url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+  },
+  {
+    titulo: 'Leao 2',
+    url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+  },
+  {
+    titulo: 'Leao 3',
     url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
   }
+  ]
 })
