e73670a5e7737ae4d26263d559d509446f8b93bb
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-04-25 13:53:48 -0300

    atalho do get do http do angular

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,11 +3,11 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
   $scope.fotos = []
 
-  var promisse = $http.get('v1/fotos')
-
-  promisse.then(function (retorno) {
-    $scope.fotos = retorno.data
-  }).catch(function (error) {
-    console.log(error)
+  $http.get('v1/fotos')
+  .success(function (fotos) {
+    $scope.fotos = fotos
+  })
+  .error(function (erro) {
+    console.log(erro)
   })
 })
