1ed17ddded1302c1d377bf10dd758d3fcbbd2262
Alvaro <alvaro.camilloneto@gmail.com>
2017-05-05 23:31:50 -0300

    implementado inclusão de foto (Post do AngularJS

---
+++ public/js/controllers/foto-controller.js
@@ -0,0 +1,18 @@
+/* global angular */
+
+angular.module('alurapic').controller('FotoController', function ($scope, $http) {
+  $scope.fotos = {}
+  $scope.submeter = function () {
+    if ($scope.formulario.$valid) {
+      $http.post('v1/fotos', $scope.foto)
+      .success(function () {
+        $scope.foto = {}
+        $scope.mensagem = 'Foto cadastrada com sucesso'
+      })
+      .error(function (erro) {
+        console.log(erro)
+        $scope.mensagem = 'Não foi possível cadastrar a foto'
+      })
+    }
+  }
+})
