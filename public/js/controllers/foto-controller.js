764fde6330c3090523411d862afc94c18e0287b8
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-05-09 17:00:49 -0300

    Refatoração para o uso do resource

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -4,37 +4,38 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http,
   $scope.fotos = {}
   $scope.mensagem = ''
 
-  var recursoFoto = $resource('v1/fotos/:fotoId')
+  var recursoFoto = $resource('v1/fotos/:fotoId', null, {
+    update: {
+      method: 'PUT'
+    }
+  })
 
   if ($routeParams.fotoId) {
-    $http.get(`v1/fotos/${$routeParams.fotoId}`)
-    .success(function (foto) {
+    recursoFoto.get({fotoId: $routeParams.fotoId},
+    function (foto) {
       $scope.foto = foto
-    })
-    .error(function (erro) {
+    }, function (erro) {
       console.log(erro)
-      $scope.mensagem = 'Não foi possível alterar a foto'
+      $scope.mensagem = 'Não foi possí­vel alterar a foto'
     })
   }
 
   $scope.submeter = function () {
     if ($scope.formulario.$valid) {
       if ($scope.foto._id) {
-        $http.put(`v1/fotos/${$scope.foto._id}`, $scope.foto)
-        .success(function () {
-          $scope.mensagem = `Foto ${$scope.foto.titulo} alterada com sucesso`
-        })
-        .error(function (erro) {
-          console.log(erro)
-          $scope.mensagem = `Não foi possível alterar a foto ${$scope.foto.titulo}`
-        })
+        recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto,
+          function () {
+            $scope.mensagem = `Foto ${$scope.foto.titulo} alterada com sucesso`
+          },
+          function (erro) {
+            console.log(erro)
+            $scope.mensagem = `NÃ£o foi possÃ­vel alterar a foto ${$scope.foto.titulo}`
+          })
       } else {
-        $http.post('v1/fotos', $scope.foto)
-        .success(function () {
+        recursoFoto.save($scope.foto, function () {
           $scope.foto = {}
           $scope.mensagem = 'Foto cadastrada com sucesso'
-        })
-        .error(function (erro) {
+        }, function (erro) {
           console.log(erro)
           $scope.mensagem = 'Não foi possível cadastrar a foto'
         })
