bf32c4bbbc1bd0c64d7cb0c21f5a6cba17f66061
Alvaro <alvaro.camilloneto@gmail.com>
2017-05-09 22:45:16 -0300

    refatorando para servicos os cadastros

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,6 @@
 /* global angular */
 
-angular.module('alurapic').controller('FotoController', function ($scope, $routeParams, recursoFoto) {
+angular.module('alurapic').controller('FotoController', function ($scope, $routeParams, cadastroDeFotos, recursoFoto) {
   $scope.fotos = {}
   $scope.mensagem = ''
 
@@ -16,24 +16,14 @@ angular.module('alurapic').controller('FotoController', function ($scope, $route
 
   $scope.submeter = function () {
     if ($scope.formulario.$valid) {
-      if ($scope.foto._id) {
-        recursoFoto.update({fotoId: $scope.foto._id}, $scope.foto,
-          function () {
-            $scope.mensagem = `Foto ${$scope.foto.titulo} alterada com sucesso`
-          },
-          function (erro) {
-            console.log(erro)
-            $scope.mensagem = `NÃ£o foi possÃ­vel alterar a foto ${$scope.foto.titulo}`
-          })
-      } else {
-        recursoFoto.save($scope.foto, function () {
-          $scope.foto = {}
-          $scope.mensagem = 'Foto cadastrada com sucesso'
-        }, function (erro) {
-          console.log(erro)
-          $scope.mensagem = 'Não foi possível cadastrar a foto'
-        })
-      }
+      cadastroDeFotos.cadastrar($scope.foto)
+                .then(function (dados) {
+                  $scope.mensagem = dados.mensagem
+                  if (dados.inclusao) $scope.foto = {}
+                })
+                .catch(function (erro) {
+                  $scope.mensagem = erro.mensagem
+                })
     }
   }
 })
